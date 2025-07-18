24d52941c7c12dfca804857cd5d5e7e4f7687612
Benjamin Owen <78978715+benowe1717@users.noreply.github.com>
2025-07-18 13:02:48 -0400

    Version 3.1 (#27)
    * updated intro wording, updated design of intro wording to wrap at the same breakpoint as the learning paths, pushed updates to css and local index testing files (#14)
    
    * Catalog page background (#16)
    
    * reworked learning paths with new responsive design that handles full screen all the way down to the md-ish breakpoint
    
    * fully reworked design based on a new reference image from marketing team, updated application learning path names, updated main.py to accommodate more variables, updated config file to allow for extra variables
    
    * fixed a bug where tailwind was not generating css properly for background images (#17)
    
    * changed pyramid image to take up 80% of the width at full screen as it was overpowering, adjusted spacing between 1. and 2.+ numbers, updated tailwind binary to watch changes made to template files and the output directory so that i do not miss the background image bug again (#18)
    
    * updated css for prod deployment
    
    * Add etm course (#20)
    
    * added ETM course to vmdr learning path -- awaiting confirmation on order
    
    * changed order of ETM course in learning path
    
    * bumped version, changed display name of etm course (#22)
    
    * Add internal learning path (#24)
    
    * bumped version, added new support learning path
    
    * change etm course name (#23)
    
    * updated intro wording, updated design of intro wording to wrap at the same breakpoint as the learning paths, pushed updates to css and local index testing files (#14)
    
    * Catalog page background (#16)
    
    * reworked learning paths with new responsive design that handles full screen all the way down to the md-ish breakpoint
    
    * fully reworked design based on a new reference image from marketing team, updated application learning path names, updated main.py to accommodate more variables, updated config file to allow for extra variables
    
    * fixed a bug where tailwind was not generating css properly for background images (#17)
    
    * changed pyramid image to take up 80% of the width at full screen as it was overpowering, adjusted spacing between 1. and 2.+ numbers, updated tailwind binary to watch changes made to template files and the output directory so that i do not miss the background image bug again (#18)
    
    * updated css for prod deployment
    
    * Add etm course (#20)
    
    * added ETM course to vmdr learning path -- awaiting confirmation on order
    
    * changed order of ETM course in learning path
    
    * bumped version, changed display name of etm course (#22)
    
    * updated main with better syntax for creating template variables, updated css with new color for internal learning paths, updated intro template to not include the levels of training for internal, updated support learning path with links, created 2nd support learning path to demo as a better alternative to what was given
    
    * updated support learning path using provided feedback, removed 2nd learning path as it was just an option
    
    * updated css
    
    * rebuilt css
    
    * updated css to include stage image link
    
    * edited some courses to remove foundation and onboarding words as they are redundant, removed WAS ilt and exam links, added TAS ilt and exam links, removed PC ilt and exam links, added PA ilt and exam links, removed PM ilt and sp links, added TE ilt and sp links, updated support catalog to match product catalog (#26)
    
    * version bump

--- src/constants/constants.py
+++ src/constants/constants.py
@@ -4,7 +4,7 @@
 # argparse
 ARGPARSE_PROGRAM_NAME = 'main.py'
 ARGPARSE_PROGRAM_DESCRIPTION = 'Build Qualys Catalog HTML file'
-ARGPARSE_VERSION = '3.0.0'
+ARGPARSE_VERSION = '3.1.0'
 ARGPARSE_AUTHOR = 'Benjamin Owen'
 ARGPARSE_REPO = 'https://github.com/benowe1717/qualys-catalog-design'
 SUPPORTED_ARGS = ['local', 'stage', 'prod']
